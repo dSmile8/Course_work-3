3f53ec81fa43811cfb9f7cb2354d3ad3d462863e
Сергей Арефьев <Simpson6515@yandex.ru>
2024-01-15 02:12:56 +0500

    added executed_operations func

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 
 def open_file():
@@ -6,4 +7,20 @@ def open_file():
         data = json.load(file)
     return data
 
+operations = open_file()
+
+def executed_operations():
+    execut_operations = []
+    for i in operations:
+        for v in i.values():
+            if v == 'EXECUTED':
+                execut_operations.append(i)
+    return execut_operations
+
+
+
+# print(executed_operations())
+# b = executed_operations()
+# for c in b:
+#     print(c)
 
