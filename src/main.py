6fb0616af42feb884b4dd49571765ad12950acf5
Сергей Арефьев <Simpson6515@yandex.ru>
2024-01-15 20:41:57 +0500

    improved function in utils.py and main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from src.utils import open_file, executed_operations, enter_transaction, get_last_values
+
+data = open_file()
+data = executed_operations(data)
+data = get_last_values(data, 5)
+data = enter_transaction(data)
+
+print('\nОперации клиента:\n')
+for i in data:
+    print(i)
--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,16 @@ from datetime import datetime
 
 
 def open_file():
+    """Открывает json файл и записывает его в переменную 'data' """
+
     with open('operations.json', 'r', encoding='UTF-8') as file:
         data = json.load(file)
     return data
 
 
 def executed_operations(data):
+    """Оставляем EXECUTED(выполненые операции) и убираем операции без поля отправитель(from)"""
+
     execut_operations = []
     empty_from = []
     for i in data:
@@ -21,41 +25,40 @@ def executed_operations(data):
     return empty_from
 
 def get_last_values(data, count_last_values):
-    data = sorted(data, key=lambda x: x["date"], reverse=True)
-    data = data[:count_last_values]
+    """Сортируем список по дате и оставляем последние несколько операций"""
+
+    data = sorted(data, key=lambda x: x["date"], reverse=True)  # сортируем список словарей по ключу 'date'
+    data = data[:count_last_values]  # берем последние count_last_values операций
     return data
 
 
 def enter_transaction(data):
+    """Создаем список строк для дальнейшего вывод в нужном нам формате"""
+
+    global from_, to_
     operations_list = []
     for i in data:
         date = datetime.fromisoformat(i['date']).strftime('%d.%m.%Y')  # меняем формат даты
         description = i['description']  # описание типа перевода
         from_last_elem = i['from'].split(' ')[-1]  # последний элемент строки "от кого"
         to_last_elem = i['to'].split(' ')[-1]  # последний элемент строки "кому"
-        if len(from_last_elem) == 16:
+        if len(from_last_elem) == 16:  # задаем условия для разных длинн счет/номер карты отправителя
             from_ = ' '.join(i['from'].split(' ')[0:-1]) + ' ' + from_last_elem[0:4] + ' ' + from_last_elem[5:7] + '**' + ' **** ' + from_last_elem[-4:]
-        elif len(from_last_elem) == 20:
+        elif len(from_last_elem) == 20:  # задаем условия для разных длинн счет/номер карты отправителя
             from_ = ' '.join(i['from'].split(' ')[0:-1]) + ' ' + '**' + from_last_elem[-4:]
         else:
             print('Длинна счета (20 цифр) либо номера карты (16 цифр) не верна')
-        if len(to_last_elem) == 16:
-            to = ' '.join(i['to'].split(' ')[0:-1]) + ' ' + to_last_elem[0:4] + ' ' + to_last_elem[5:7] + '**' + ' **** ' + to_last_elem[-4:]
-        elif len(to_last_elem) == 20:
-            to = ' '.join(i['to'].split(' ')[0:-1]) + ' ' + '**' + to_last_elem[-4:]
+        if len(to_last_elem) == 16:  # задаем условия для разных длинн счет/номер карты получателя
+            to_ = ' '.join(i['to'].split(' ')[0:-1]) + ' ' + to_last_elem[0:4] + ' ' + to_last_elem[5:7] + '**' + \
+                 ' **** ' + to_last_elem[-4:]
+        elif len(to_last_elem) == 20:  # задаем условия для разных длинн счет/номер карты получателя
+            to_ = ' '.join(i['to'].split(' ')[0:-1]) + ' ' + '**' + to_last_elem[-4:]
         else:
             print('Длинна счета (20 цифр) либо номера карты (16 цифр) не верна')
-        amount = i['operationAmount']['amount']
-        currency = i['operationAmount']['currency']['name']
-        operations_list.append(f'{date} {description}\n{from_} -> {to}\n{amount} {currency} \n\n ')
+        amount = i['operationAmount']['amount']  # сумма перевода
+        currency = i['operationAmount']['currency']['name']  # валюта
+        operations_list.append(f'{date} {description}\n{from_} -> {to_}\n{amount} {currency} \n\n ')
     return operations_list
-
-# print(enter_transaction(data))
-
-def date_sort(data):
-    pass
-
-
 #
 #
 #
