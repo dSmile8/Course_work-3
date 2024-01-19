9a088909a18db6020576fcb6b1ed7711ca6fd80c
Сергей Арефьев <Simpson6515@yandex.ru>
2024-01-19 23:08:18 +0500

    fix CODE

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime
-from path import Path
+from pathlib import Path
 
 DATA_DIR = Path(__file__).parent.parent.joinpath('operations.json')
 def open_file():
@@ -29,30 +29,56 @@ def get_last_values(data, count_last_values):
     return data
 
 
+def format_date(data):
+    """Меняем формат даты в файле"""
+
+    for i in data:
+        i['date'] = datetime.fromisoformat(i['date']).strftime('%d.%m.%Y')
+    return data
+
+def hide_sender_number(data):
+    """Маскирует номер карты/счета отправителя"""
+
+    for i in data:
+        if 'from' in i:
+            sender = i['from'].split()  # получаем список с информацией об отправителе из строки
+            sender_number = sender[-1]  # получаем номер отправителя из списка
+            if len(sender_number) == 16:
+                hide_sender_number = f"{' '.join(sender[0:-1])} {sender_number[:4]} {sender_number[4:6]}** **** {sender_number[-4:]}"
+                i['from'] = hide_sender_number
+            elif len(sender_number) == 20:
+                hide_sender_number = f"{' '.join(sender[0:-1])} **{sender_number[-4:]}"
+                i['from'] = hide_sender_number
+    return data
+
+
+def hide_recipient_number(data):
+    """Маскирует номер карты/номера получателя"""
+
+    for i in data:
+        recipient = i['to'].split()  # получаем список с информацией об получателе из строки
+        recipient_number = recipient[-1]  # получаем номер отправителя из списка
+        if len(recipient_number) == 16:
+            hide_recipient_number = f"{' '.join(recipient[0:-1])} {recipient_number[:4]} {recipient_number[4:6]}** **** {recipient_number[-4:]}"
+            i['to'] = hide_recipient_number
+        elif len(recipient_number) == 20:
+            hide_recipient_number = f"{' '.join(recipient[0:-1])} **{recipient_number[-4:]}"
+            i['to'] = hide_recipient_number
+    return data
+
 def enter_transaction(data):
-    """Создаем список строк для дальнейшего вывод в нужном нам формате"""
+    """Создаем список строк для дальнейшего вывода в нужном нам формате"""
 
     operations_list = []
     for i in data:
-        date = datetime.fromisoformat(i['date']).strftime('%d.%m.%Y')  # меняем формат даты
-        description = i['description']                                 # описание типа перевода
-        operations_amount = f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}"
+        date = i['date']
+        description = i['description']
+        operations_amount = i['operationAmount']['amount'] + ' ' + i['operationAmount']['currency']['name']
+        to_info = i['to']
         if 'from' in i:
-            sender = i['from'].split()  # получаем список с информацией об отправителе из строки
-            recipient = i['to'].split()  # получаем список с информацией об получателе из строки
-            sender_number = sender.pop()  # получаем номер отправителя из списка
-            recipient_number = recipient.pop()  # получаем номер отправителя из списка
-            if len(sender_number) == 16:
-                hide_sender_number = f"{sender_number[:4]} {sender_number[4:6]}** **** {sender_number[-4:]}"
-            elif len(sender_number) == 20:
-                hide_sender_number = f"**{sender_number[-4:]}"
-            if len(recipient_number) == 16:
-                hide_recipient_number = f"{recipient_number[:4]} {recipient_number[4:6]}** **** {recipient_number[-4:]}"
-            elif len(recipient_number) == 20:
-                hide_recipient_number = f"**{recipient_number[-4:]}"
-            to_info = f"{' '.join(recipient)} {hide_recipient_number}"
-            from_info = f"{' '.join(sender)} {hide_sender_number}"
+            from_info = i['from']
+            to_info = i['to']
+            operations_list.append(f'{date} {description}\n{from_info} -> {to_info}\n{operations_amount}\n\n ')
         else:
-            from_info, to_info = '', ''
-        operations_list.append(f'{date} {description}\n{from_info} -> {to_info}\n{operations_amount}\n\n ')
+            operations_list.append(f'{date} {description}\n{to_info}\n{operations_amount}\n\n ')
     return operations_list
